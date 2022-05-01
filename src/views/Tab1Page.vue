86a073324730e1767af921750e18c036f027bee2
Samuele Monasterolo <smonasterolo2@gmail.com>
2022-05-01 16:56:31 +0200

    Revert "init github"
    This reverts commit e601e1851f79d90719724d154ff9110c87a93748.

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,41 +2,28 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Fact Container</ion-title>
+        <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Cat Fact</ion-title>
+          <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <CatFactContainer />
+    
+      <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from "@ionic/vue";
-import CatFactContainer from "@/components/CatFactContainer.vue";
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
 
-export default defineComponent({
-  name: "Tab1Page",
-  components: {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonPage,
-    CatFactContainer,
-  },
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 });
 </script>
