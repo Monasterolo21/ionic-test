ed56909719e7d64c9bf4c3638a5fdd451514e989
Samuele Monasterolo <smonasterolo2@gmail.com>
2022-05-01 18:42:42 +0200

    adding dist subtree

--- js/78.88d18b30.js
+++ js/78.88d18b30.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const i=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,i=document.elementFromPoint(e/2,n/2);if(!i)return;const c=(0,o.a)(i);c&&new Promise((t=>(0,r.c)(c,t))).then((()=>{(0,s.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,o.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.05ef9b49.js.map
+//# sourceMappingURL=78.88d18b30.js.map
