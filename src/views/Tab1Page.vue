039601442460de7f6a7b78310c0d6d578247fc82
Samuele Monasterolo <smonasterolo2@gmail.com>
2022-05-01 17:04:49 +0200

    test

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Cat Fact</ion-title>
+          <ion-title size="large">Cat Facts</ion-title>
         </ion-toolbar>
       </ion-header>
 
