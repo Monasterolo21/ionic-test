d96f35a6dc4a4c91a10462e9bed15541d9951e18
Samuele Monasterolo <smonasterolo2@gmail.com>
2022-05-01 17:00:25 +0200

    Revert "Revert "init github""
    This reverts commit 86a073324730e1767af921750e18c036f027bee2.

---
+++ src/components/CatFactContainer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="container">
+    <strong>{{ title }}</strong>
+    <p>
+      {{ catFact }}
+    </p>
+
+    <ion-button color="success" @click="fetchCatFact">
+      Get Cat Fact
+    </ion-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "CatFactContainer",
+  props: {
+    title: String,
+  },
+
+  components: {
+    IonButton,
+  },
+
+  data() {
+    return {
+      catFact: "",
+    };
+  },
+
+  methods: {
+    async fetchCatFact() {
+      const response = await fetch(
+        "https://cat-fact.herokuapp.com/facts/random"
+      );
+      const data = await response.json();
+      this.catFact = data.text;
+    },
+  },
+
+  mounted() {
+    this.fetchCatFact();
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 2em;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,28 +2,41 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Fact Container</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Cat Fact</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <CatFactContainer />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import CatFactContainer from "@/components/CatFactContainer.vue";
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+export default defineComponent({
+  name: "Tab1Page",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    CatFactContainer,
+  },
 });
 </script>
